49f12b4658a11a0e0ac4580a41b56114811f3abe
ErojiAruna <aruna.eroji@gamil.com>
2023-07-12 14:21:01 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading = React.createElement(
+  'h1',
+  {
+    id: 'title',
+  },
+  'Namaste 🙏'
+);
+
+const heading2 = React.createElement(
+  'h2',
+  {
+    id: 'title',
+  },
+  'Namaste!'
+);
+
+const container = React.createElement(
+  'div',
+  {
+    id: 'container',
+  },
+  [heading, heading2]
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// passing a react element inside the root
+root.render(container);
