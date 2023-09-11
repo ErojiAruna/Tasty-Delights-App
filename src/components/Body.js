33f35345cc5e7b41561db2f6cfe4812b3148cde5
ErojiAruna <aruna.eroji@gamil.com>
2023-09-11 23:17:09 +0530

    initial commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,54 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { restaurantList } from '../config';
 import RestaurantCard from './RestaurantCard';
+import { GET_RES_API } from '../config';
+import Shimmer from './Shimmer';
 
 function filterData(searchText, restaurants) {
   const filterData = restaurants.filter((restaurant) =>
-    restaurant.data.name.includes(searchText)
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
   );
 
   return filterData;
 }
 
 const Body = () => {
-  //  const searchTxt = 'KFC';
-  const [restaurants, setRestaurants] = useState(restaurantList);
-  const [searchText, setSearchText] = useState(''); // To create state varialble
-  console.log(restaurants);
+  const [allRestaurants, setAllRestaurants] = useState([]);
 
-  return (
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState(''); // To create state varialble
+
+  // empty dependency array => once after render
+  // dependency array [searchText] => once after initial render + everytime after re-render (my searchText changes)
+  useEffect(() => {
+    // API call
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(GET_RES_API);
+    const json = await data.json();
+    console.log(json);
+    // This isa bad way to do // Optional Chaining
+    setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  console.log('render');
+
+  // Conditional Rendering
+  // if restaurant is empty => shimmer Ui
+  // if restaurant has data => actual data UI
+
+  // not render component (Early return)
+  if (!allRestaurants) return null;
+
+  if (filteredRestaurants?.length === 0)
+    return <h1>No Restaurant match your Filter!!</h1>;
+
+  return allRestaurants?.length === 0 ? (
+    <Shimmer />
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -34,16 +66,18 @@ const Body = () => {
           onClick={() => {
             // need to filter the data
             // update the state - restaurants
-            const data = filterData(searchText, restaurants);
-            setRestaurants(data);
+            const data = filterData(searchText, allRestaurants);
+            setFilteredRestaurants(data);
           }}
         >
           Search
         </button>
       </div>
       <div className="restaurant__list">
-        {restaurants.map((restaurant, index) => {
-          return <RestaurantCard {...restaurant.data} key={index} />;
+        {filteredRestaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
         })}
       </div>
     </>
